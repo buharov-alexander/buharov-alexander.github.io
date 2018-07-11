72104b7eededed2d231b46f1bde632154ab134fd
buharov-alexander <buharovsasha@yandex.ru>
2018-07-11 22:58:36 +0700

    Add navigation bar and banner

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(window).on("scroll", function() {
+  $(window).scrollTop() >= 50 ? $("#mainNav").addClass("stickyadd") : $("#mainNav").removeClass("stickyadd")
+}),
+$(".scroll-down a").on("click", function(e) {
+  var t = $(this);
+  $("html, body").stop().animate({
+      scrollTop: $(t.attr("href")).offset().top - 0
+  }, 1500, "easeInOutExpo"),
+  e.preventDefault()
+})
